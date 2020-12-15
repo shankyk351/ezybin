087a44547173ca5053a236a9670f83297a2d5119
Himanshu Gupta <shankyk351@gmail.com>
2020-12-15 19:20:24 +0530

    responsive fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,16 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  $('#mySidenav').css('width', '250px');
   $('.overlay').show();
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  $('#mySidenav').css('width', 0);
   $('.overlay').hide();
 }
 
 $(document).ready(function(){
   $('.overlay').click(function(){
     $('.overlay').hide();
-    document.getElementById("mySidenav").style.width = "0";
-    // $('.sidenav').animate('width', 0);
+    $('#mySidenav').css('width', 0);
   })
 })
