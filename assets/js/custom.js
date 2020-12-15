adc4301ccbd2940ed0d8c1a2dedb4fd25e1bc25a
Himanshu Gupta <shankyk351@gmail.com>
2020-12-15 18:51:11 +0530

    client changes implemented

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,17 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";4
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  $('.overlay').show();
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  $('.overlay').hide();
+}
+
+$(document).ready(function(){
+  $('.overlay').click(function(){
+    $('.overlay').hide();
+    document.getElementById("mySidenav").style.width = "0";
+    // $('.sidenav').animate('width', 0);
+  })
+})
